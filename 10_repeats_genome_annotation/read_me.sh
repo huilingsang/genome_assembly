c3c9c8bb6c14d4c4d95324a2d94cc189fb53c605
huilingsang <huilingsang2020@gmail.com>
2026-02-02 17:51:20 +1300

    add 10_repeats_genome_annotation scripts

---
+++ 10_repeats_genome_annotation/read_me.sh
@@ -0,0 +1,6 @@
+#for repeats masking and genome annotation, please refer to the Github
+# repeat masking
+https://github.com/carla-hazelf/hakuRaukura_genomeAssembly_pipeline/blob/main/1.4.0_Repeat_Masking.md
+
+#genome annotation
+https://github.com/carla-hazelf/hakuRaukura_genomeAssembly_pipeline/blob/main/1.5.0_Annotation.md
